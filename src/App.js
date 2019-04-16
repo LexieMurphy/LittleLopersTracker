b4dc43073ec0d18c023c0ca5f8ac0f49fccba64b
Tiff L <tifftiffnlewis@gmail.com>
2019-04-15 21:14:51 -0500

    file reorganization with yarn and adding OKTA log in

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import { ImplicitCallback } from '@okta/okta-react';
+import {
+  CssBaseline,
+  withStyles,
+} from '@material-ui/core';
+
+import AppHeader from './components/AppHeader';
+import Home from './pages/Home';
+
+const styles = theme => ({
+  main: {
+    padding: 3 * theme.spacing.unit,
+    [theme.breakpoints.down('xs')]: {
+      padding: 2 * theme.spacing.unit,
+    },
+  },
+});
+
+const App = ({ classes }) => (
+  <Fragment>
+    <CssBaseline />
+    <AppHeader />
+    <main className={classes.main}>
+    <Route exact path="/" component={Home} />
+    <Route path="/implicit/callback" component={ImplicitCallback} />
+    </main>
+  </Fragment>
+);
+
+export default withStyles(styles)(App);
---
+++ src/components/AppHeader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  withStyles,
+} from '@material-ui/core';
+
+  import LoginButton from './LoginButton';
+
+  const styles ={
+    flex: {
+      flex: 1,
+    },
+  };
+
+  const AppHeader = ({classes}) => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        My React App
+      </Typography>
+      <div className={classes.flex} />
+      <LoginButton />
+    </Toolbar>
+  </AppBar>
+);
+
+export default withStyles(styles)(AppHeader);
