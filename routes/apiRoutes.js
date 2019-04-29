f7347220de0f658f7a2a179b76c0a19ccef23c5e
schmitzaddie <schmitzaddie@gmail.com>
2019-04-29 18:06:28 -0500

    Additional apiRoutes udpates.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/loperItems", function (req, res) {
-        res.end('hello')
-        // db.loperItems.findAll({})
-        //     .then(function (dbloperItems) {
-        //         res.json(dbloperItems);
-        //     });
+        db.loperItems.findAll({})
+            .then(function (dbloperItems) {
+                res.json(dbloperItems);
+            });
     });
 };
