b6eb03971e0c1c227602e208f5bb9dd28eac6dbe
Tiff L <tifftiffnlewis@gmail.com>
2019-05-06 16:18:41 -0500

    added theme component

---
+++ client/src/components/Theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from 'material-ui/styles';
+import indigo from 'material-ui/colors/indigo';
+import purple from 'material-ui/colors/purple';
+
+export default createMuiTheme({
+    palette: {
+        primary: purple,
+        secondary: indigo
+    }
+});
